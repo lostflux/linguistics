1af909e2a6d5cff3c2d01e1434a88e33f612e407
siavava <amittaijoel@outlook.com>
2024-01-27 22:07:23 -0500

    hw2 update

--- P2/part-1/naive_bayes.py
+++ P2/part-1/naive_bayes.py
@@ -16,69 +16,70 @@ from collections import defaultdict, Counter
 from math import log
 from operator import itemgetter # you may find this helpful
 from typing import *
-def print_results(priors: DefaultDict[str, float], likelihoods: DefaultDict[str, DefaultDict[str, float]]) -> None:
-    """
-    Prints the priors and likelihoods in a readable format.
-    """
-    print("PRIORS")
-    for c, p in priors.items():
-        print(f"{c}: {p:.10f}")
-    print("\n\nLIKELIHOODS")
-    for f, d in likelihoods.items():
-        for c, p in d.items():
-            print(f"{f} | {c}: {p:.10f}")
+import pandas as df
 
 class NaiveBayes():
 
     def __init__(self):
         self.prior = defaultdict(float)
         self.likelihood = defaultdict(lambda: defaultdict(float))
-        self.total_words = None
+        
+        #? other instance vars
+        # self.word_counts = Counter()      
+        self.vocab_size = Counter()
+        self.vocab = set()
+        self.labels = set()
+        self.words = defaultdict(lambda: defaultdict(int))
+        
+        # self.vocab_size = 0
 
 
-    def train(self, train_set):
+    def train(self, train_set: str) -> None:
         """
         Trains a Naive Bayes classifier on a training set.
         Specifically, fills in self.prior and self.likelihood such that:
         self.prior[class] = log(P(class))
         self.likelihood[feature][class] = log(P(feature|class))
         """
-        doc_count = defaultdict(int)
-        word_count = defaultdict(lambda : defaultdict(int))
-        total_words = defaultdict(int)
+        document_counts = Counter()
+        class_word_counts = defaultdict(Counter)
 
-        # iterate over training documents
+        #? accumulate counts
         for root, dirs, files in os.walk(train_set):
             for name in files:
-                class_label = root.split('/')[-1]
-                # print("current class label is: {}".format(class_label))
-                doc_count[class_label] += 1
+                label = root.split('/')[-1]
+                self.labels.add(label)
+                document_counts[label] += 1
                 with open(os.path.join(root, name)) as f:
-                    contents = f.read().split()
-                    for word in contents:
-                        word_count[class_label][word] += 1
-                        total_words[class_label] += 1
+                    for word in f.read().split():
+                        self.vocab.add(word)
+                        class_word_counts[label][word] += 1
 
-        # normalize counts to probabilities, and take logs
-        # let's now do log probabilities
-        total = sum(doc_count.values())
+        #? norm to log probabilities
+        self.vocab_size = { label: sum(class_word_counts[label].values()) for label in document_counts }
+        self.vocab_size["total"] = len(self.vocab)
+        
+        document_counts_sum = sum(document_counts.values())
         self.prior = {
-            class_label: log(doc_count[class_label]/total)
-            for class_label in doc_count
+            label: log(document_counts[label] / document_counts_sum)
+            for label in document_counts
         }
         
-        for class_label in doc_count:
-            total_word_count = total_words[class_label] + len(word_count[class_label])
-            for word in word_count[class_label]:
-                self.likelihood[word][class_label] = log((word_count[class_label][word]+1)/total_word_count)
+        for word in self.vocab:
+            for label in self.labels:
+                numerator = class_word_counts[label][word] + 1
+                denominator = self.vocab_size[label] + self.vocab_size["total"]
+                self.likelihood[word][label] = log(numerator / denominator)
+                
+        
+        for label in document_counts:
+            self.words[label] = {
+                "total": sum(class_word_counts[label].values()),
+                "unique": len(class_word_counts[label])
+            }
 
-        # updated global variables
-        self.total_words = total_words
-        self.vocabulary_size = {class_label: len(word_count[class_label]) for class_label in word_count}
 
-        print_results(self.prior, self.likelihood)
-
-    def test(self, dev_set):
+    def test(self, dev_set: str) -> Dict[str, Dict[str, str]]:
         """
         Tests the classifier on a development or test set.
         Returns a dictionary of filenames mapped to their correct and predicted
@@ -86,31 +87,33 @@ class NaiveBayes():
         results[filename]["correct"] = correct class
         results[filename]["predicted"] = predicted class
         """
-        results = defaultdict(dict)
-        # iterate over testing documents
-        for root, directories, files in os.walk(dev_set):
-            for name in files:
-                with open(os.path.join(root, name)) as f:
-                    # calculate log-probabilities for each class
-                    # from log-probabilities, get most likely class
-                    words = f.read().split()
-                    class_score = {class_label : self.prior[class_label] for class_label in self.prior}
-                    # calculate log-probabilities for each class
-                    for word in words:
-                        for class_label in self.prior:
-                            total_word_count = self.total_words[class_label]
-                            vocabulary_size = self.vocabulary_size[class_label]
-                            word_log_probability = self.likelihood[word].get(class_label, log(1/(total_word_count+vocabulary_size)))
-                            class_score[class_label] += word_log_probability
-                    # from log-probabilities, get most likely class
-                    predicted_most_likely_class = max(class_score.items(), key=itemgetter(1))[0]
-                    correct_class = root.split('/')[-1]
-                    # print("predicted_most_likely_class: {}, correct_class: {}".format(predicted_most_likely_class, correct_class))
-                    results[name] = {'correct':correct_class, 'predicted':predicted_most_likely_class}
         
+        results = {}
+        for root, directories, files in os.walk(dev_set):
+            
+            for filename in files:
+                actual = root.split('/')[-1]
+                # print(f"{actual = }")
+                with open(os.path.join(root, filename)) as f:
+                    
+                    sequence = f.read().split()
+                    sums = self.prior.copy()
+                    
+                    for word in sequence:
+                        for label in sums:
+                            if word in self.vocab:
+                                sums[label] += self.likelihood[word][label]
+                            else:
+                                sums[label] += log(1 / (self.words[label]["total"] + self.vocab_size["total"]))
+
+                    results[filename] = {
+                        "correct": actual,
+                        "predicted": max(sums.items(), key=itemgetter(1))[0]
+                    }
+                    
         return results
 
-    def evaluate(self, results):
+    def evaluate(self, results: Dict[str, Dict[str, str]]) -> None:
         """
         Given results, calculates the following:
         Precision, Recall, F1 for each class
@@ -118,34 +121,95 @@ class NaiveBayes():
         Also, prints evaluation metrics in readable format.
         """
         # you may find this helpful
-        confusion_matrix = defaultdict(lambda: defaultdict(int))
+        CM = defaultdict(Counter)
         for result in results.values():
-            confusion_matrix[result['correct']][result['predicted']] += 1
-        for class_label in confusion_matrix:
-            true_positive = confusion_matrix[class_label][class_label]
-            false_positive = sum(
-                confusion_matrix[other][class_label] for other in confusion_matrix if other != class_label)
-            false_negative = sum(
-                confusion_matrix[class_label][other] for other in confusion_matrix if other != class_label)
-
-            precision = true_positive / (true_positive + false_positive) if true_positive + false_positive > 0 else 0
-            recall = true_positive / (true_positive + false_negative) if true_positive + false_negative > 0 else 0
-            f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
-
-            print("Class: {}, Precision: {}, Recall: {}, F1 Score: {}".format(class_label, precision, recall, f1))
-
-        total_correct = sum(confusion_matrix[class_label][class_label] for class_label in confusion_matrix)
-        total_predictions = sum(sum(confusion_matrix[class_label].values()) for class_label in confusion_matrix)
-        accuracy = total_correct / total_predictions if total_predictions > 0 else 0
-        print("Overall Accuracy: {}".format(accuracy))
-
-        print("Confusion Matrix: {}".format(confusion_matrix))
+            actual = result['correct']
+            predicted = result['predicted']
+            CM[actual][predicted] += 1
+            
+        res = {}
+        
+        for class_name in CM:
+            TP = CM[class_name][class_name]
+            FP = 0
+            for other_class in CM:
+                if other_class != class_name:
+                    FP += CM[other_class][class_name]
+                    
+            FN = 0
+            for other_class in CM:
+                if other_class != class_name:
+                    FN += CM[class_name][other_class]
+                    
+            TN = 0
+            for other_class in CM:
+                for other_class2 in CM:
+                    if class_name not in (other_class, other_class2):
+                        TN += CM[other_class][other_class2]
+                        
+                    
+            precision = recall = f1 = accuracy = specificity = 0
+            if TP + FP > 0:
+                precision = TP / (TP + FP)
+                
+            if TP + FN > 0:
+                recall = TP / (TP + FN)
+                
+            if precision + recall > 0:
+                f1 = 2 * precision * recall / (precision + recall)
+                
+            if TP + FP + TN + FN > 0:
+                accuracy = (TP + TN) / (TP + FP + TN + FN)
+                
+            if TN + FP > 0:
+                specificity = TN / (TN + FP)
+            
+            res[class_name] = {
+                "metrics": df.DataFrame(
+                    [[precision, recall, f1, accuracy, specificity]],
+                    index=["MEASURE"],
+                    columns=["PRECISION", "RECALL", "F1", "ACCURACY", "SPECIFICITY"]
+                ),
+                "CM": df.DataFrame(
+                    [[TP, FP], [FN, TN]],
+                    index=["ACTUAL POSITIVE", "ACTUAL NEGATIVE"],
+                    columns=["PREDICTED POSITIVE", "PREDICTED NEGATIVE"]
+                )
+            }
+            
+        self.print_results(res)
+            
+    def print_results(self, res: Dict[str, Dict[str, df.DataFrame]]) -> None:
+        for class_name in res:
+            print(f"\n\n\n\n") # some space for distinction
+            print(f"-------------------------------------------------------------")
+            print(f"CLASS: {class_name}")
+            print(f"-------------------------------------------------------------")
+            print(f"\n-------------------------------------------------------------")
+            print(f"METRICS")
+            print(f"-------")
+            print(f"{res[class_name]['metrics']}")
+            print(f"-------------------------------------------------------------")
+            print(f"\n-------------------------------------------------------------")
+            print(f"CONFUSION MATRIX")
+            print(f"----------------")
+            print(f"{res[class_name]['CM']}")
+            print(f"-------------------------------------------------------------")
 
 if __name__ == "__main__":
     nb = NaiveBayes()
     # make sure these point to the right directories
-    #nb.train("movie_reviews/train")
-    nb.train("movie_reviews_small/train")
-    #results = nb.test("movie_reviews/dev")
-    results = nb.test("movie_reviews_small/test")
+    
+    #! -------------------------------------
+    
+    #? small dataset
+    # nb.train("movie_reviews_small/train")
+    # results = nb.test("movie_reviews_small/test")
+    
+    #? main dataset
+    nb.train("movie_reviews/train")
+    results = nb.test("movie_reviews/dev")
+    
+    #! -------------------------------------
+    
     nb.evaluate(results)
